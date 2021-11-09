ff55ad6e533b4af16d98085ee7f57cf5310af45f
yogesh P <yogesh.padmanaban@galaxyweblinks.in>
2021-11-09 14:58:12 +0530

    changes

--- src/services/auth.service.js
+++ src/services/auth.service.js
@@ -1,12 +1,12 @@
 import axios from "axios";
 
-const API_URL = "http://localhost:8080/api/auth/";
+const API_URL = "http://127.0.0.1:8000/";
 
 class AuthService {
   login(username, password) {
     console.log(username);
     return axios
-      .post(API_URL + "signin", { username, password })
+      .post(API_URL + "login", { username, password })
       .then((response) => {
         if (response.data.accessToken) {
           localStorage.setItem("user", JSON.stringify(response.data));
